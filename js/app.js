75c533769361c9852cad981964ea947a5c7acd26
Leonardo Grigoletto <grigolettodev@gmail.com>
2025-04-08 09:51:37 -0300

    Pega valores

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+
+function comprar (){
+
+    //Pegar valores
+    //Subtrair valores
+    //Verificar se foram esgotados
+
+}
+
+function pegaValores(id){
+
+    let elementoHtml = document.getElementById(id);
+    let valor = elementoHtml.value;
+    return valor;
+}
