827ba1a097e31241b421b5f33fa9e8a8075c9696
Leonardo Grigoletto <grigolettodev@gmail.com>
2025-04-08 10:07:16 -0300

    Variaveis Globais

--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
 
+//Variáveis globais
+let elementoQtdInferior = document.getElementById("qtd-inferior");
+let elementoQtdSuperior = document.getElementById("qtd-superior");
+let elementoQtdPista = document.getElementById("qtd-pista");
+
+let qtdInferior = parseInt(elementoQtdInferior.textContent);
+let qtdSuperior = parseInt(elementoQtdSuperior.textContent);
+let qtdPista = parseInt(elementoQtdPista.textContent);
+
+
 
 function comprar (){
 
     //Pegar valores
+    let tipoIngresso = pegaValores("tipo-ingresso");
+    let qtdIngresso = parseInt(pegaValores("qtd"));
+    console.log(tipoIngresso);
+    console.log(qtdIngresso);
     //Subtrair valores
     //Verificar se foram esgotados
 
